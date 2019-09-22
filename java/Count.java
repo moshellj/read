45ce59c5c97118b3063c1109d304ddfe7f1e654b
Jack Moshell <jack.moshell@live.mercer.edu>
2019-09-21 22:52:15 -0400

    initial java commit

---
+++ java/Count.java
@@ -0,0 +1,10 @@
+
+class Count{
+	public int words;
+	public int sylls;
+	public int sents;
+	public String[] wordList;
+	
+	public Count(){}//default constructor
+	
+}
---
+++ java/Scores.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Scores
+{
+public static void main(String args[]){
+	Count test = new Count();
+	test.words = 7;
+	System.out.println(test.words);
+}
+//public static Count tally(){
+	
+//}
+}
