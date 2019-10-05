fabfeaf6adbb18d684fb8a921955fe09ed75bf5b
Jack Moshell <jack.moshell@live.mercer.edu>
2019-10-05 14:31:50 -0400

    subroutine fix_token working

--- fortran/scores.f95
+++ fortran/scores.f95
@@ -12,6 +12,9 @@ program scores
             character(:), allocatable :: filename, string
             integer(KIND=4) :: filesize
         end subroutine
+        subroutine fix_token(string)
+            character(:), allocatable :: string
+        end subroutine fix_token
     end interface
     
     filename = "/home/moshell_jw/alice.txt"!replace with argument
@@ -24,7 +27,8 @@ program scores
     
     do while (len(outline) .ne. 0)
         call get_next_token(string, outline, token)
-        print *, token
+        call fix_token(token)
+        print *, token, len(token)
         string = outline
     enddo
     
@@ -85,3 +89,33 @@ subroutine read_file(filename, string, filesize)
     counter = counter-1
     close(5)
 end subroutine read_file
+
+! modifies a token string for ease of parsing.
+! strips nonletters except for ' (and thus may return an empty string)
+! be sure to reason about sentence-ending before calling this
+! normalizes to lowercase. Elemental, for chall list.
+subroutine fix_token(string)
+    character(:), allocatable :: string
+    character(:), allocatable :: stripset
+    integer :: writehead, readhead
+    stripset = "!@#$%&^*()_+-=[]\{}|;" // '"' // ":,./<>?`~"
+    !strip: rewrite string to itself. if invalid character, do not write, do not
+    !       move write head. trim end.
+    writehead = 1
+    readhead = 1
+    do while (readhead <= len(string))
+        if ( scan(string(readhead:readhead), stripset) > 0 ) then
+            ! do nothing
+        else
+            !lowercasize: if 65 <= c <= 90, add 32
+            if ( 65 <= iachar(string(readhead:readhead)) .and. iachar( &
+                string(readhead:readhead)) <= 90) then
+                string(readhead:readhead) = achar(iachar(string(readhead:readhead)) + 32)
+            end if
+            string(writehead:writehead) = string(readhead:readhead)
+            writehead = writehead + 1
+        end if
+        readhead = readhead + 1
+    enddo
+    string = string(1:writehead - 1)
+end subroutine fix_token
