9720a5e9551e4847ededbe85acc0449a46ddda28
Jack Moshell <jack.moshell@live.mercer.edu>
2019-10-08 11:55:59 -0400

    file readin

---
+++ ada/flesch.adb
@@ -0,0 +1,29 @@
+with Ada.Text_IO;
+use Ada.Text_IO;
+with Ada.Characters.Latin_1;
+with Ada.Diretories;
+
+procedure flesch is
+
+	package AIO renames Ada.Text_IO;
+	package ACL renames Ada.Characters.Latin_1;
+
+	In_File	: aio.File_Type;
+	value	: Character;
+	pos		: Integer;
+	s: string(1..10000);
+
+begin
+	
+	AIO.Open(File => In_File, Mode => AIO.In_File, Name => "/home/moshell_jw/alice.txt");
+	
+	pos := 0;
+	while not aio.End_Of_File(In_File) loop
+		aio.get(File => In_File, Item => value);
+		AIO.Put(Item => value);
+	end loop;
+	
+	exception
+		when aio.end_error => aio.close(file => In_file);
+
+end flesch;
