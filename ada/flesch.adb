f576a19dd93fc34f7eb973ee10026a96a50228cf
Jack Moshell <jack.moshell@live.mercer.edu>
2019-10-11 17:48:57 -0400

    rebased on given tokenizer

--- ada/flesch.adb
+++ ada/flesch.adb
@@ -1,34 +1,58 @@
-with Ada.Text_IO;
+with ada.text_io;
 use Ada.Text_IO;
-with Ada.Characters.Latin_1;
-with Ada.Directories;
-with ada.direct_io;
+with ada.io_exceptions;
+use Ada.IO_Exceptions;
+with Ada.Containers.Indefinite_Vectors;
+use  Ada.Containers;
+with ada.strings.maps;
+with ada.strings.fixed;
 
 procedure flesch is
-
-	package AIO renames Ada.Text_IO;
-	package ACL renames Ada.Characters.Latin_1;
-
-	In_File	: aio.File_Type;
-	--value	: Character;
-	pos		: Integer;
-	filename : string := "/home/moshell_jw/alice.txt";
-	linesize : natural;
-	s: string(1..10000);
+	
+	package ASM renames ada.strings.maps;
+   In_File      : Ada.Text_IO.File_Type;
+   value        : Character;
+   string_array : array (1..2000000) of Character;
+   pos          : Integer;
+   package String_Vector is new Indefinite_Vectors (Natural,String); use String_Vector;
+   s       : String(1..2000000) := (others => Character'Val(0));
+   current : Positive := s'First;
+   v       : Vector;
 
 begin
-	
-	AIO.Open(File => In_File, Mode => AIO.In_File, Name => filename);
-	
-	pos := 0;
-	while not aio.End_Of_File(In_File) loop
-		s := (others => acl.nul);
-		--aio.get(File => In_File, Item => value);
-		aio.get_line(file => in_file, item => s, last => linesize);
-		AIO.Put(Item => s & ACL.lf);
-	end loop;
-	
-	exception
-		when aio.end_error => aio.close(file => In_file);
+
+   Ada.Text_IO.Put (Item => "Done initilizing variables");
+   Ada.Text_IO.Open (File => In_File, Mode => Ada.Text_IO.In_File, Name => "/home/moshell_jw/alice.txt");
+
+	--for each
+   pos := 0;
+   while not Ada.Text_IO.End_Of_File(In_File) loop
+    Ada.Text_IO.Get (File => In_File, Item => value);
+    pos := pos + 1;
+    string_array(pos) := value;
+   end loop;
+   exception
+   when Ada.IO_Exceptions.END_ERROR => Ada.Text_IO.Close (File => In_File);
+
+
+   for i in string_array'range loop
+     s(i) := string_array(i);
+   end loop;
+
+	--separate
+  for i in s'range loop
+    --if s (i) = ' ' or i = s'last then
+    if ASM.is_in(s(i), ASM.to_set(" !.?;:")) then
+      v.append (s(current .. i));
+      current := i + 1;
+    end if;
+   end loop;
+
+  for s of v loop
+   put(s);
+   new_line;
+  end loop;
+
+   Ada.Text_IO.New_Line;
 
 end flesch;
