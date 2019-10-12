b08f0937c36bc301aec3110bf9c0037c43ed5996
Jack Moshell <jack.moshell@live.mercer.edu>
2019-10-11 23:47:19 -0400

    almost works

--- ada/flesch.adb
+++ ada/flesch.adb
@@ -6,23 +6,39 @@ with Ada.Containers.Indefinite_Vectors;
 use  Ada.Containers;
 with ada.strings.maps;
 with ada.strings.fixed;
+with ada.characters.handling;
+with ada.integer_text_io;
+use ada.integer_text_io;
+with ada.float_text_io;
+with ada.command_line;
 
 procedure flesch is
 	
 	package ASM renames ada.strings.maps;
+	package ASF renames ada.strings.fixed;
+	package ACH renames ada.characters.handling;
    In_File      : Ada.Text_IO.File_Type;
+	filename : string(1..10000);
    value        : Character;
    string_array : array (1..2000000) of Character;
    pos          : Integer;
    package String_Vector is new Indefinite_Vectors (Natural,String); use String_Vector;
    s       : String(1..2000000) := (others => Character'Val(0));
+	word   : string(1..31) := (others => Character'val(0));
    current : Positive := s'First;
    v       : Vector;
+	validsent, validword, lchvow : Boolean := False;
+	sentcount, wordcount, syllcount, syllinword: natural := 0;
+	writeh : natural;
+	alpha, beta, result: float := 0.0;
 
 begin
-
-   Ada.Text_IO.Put (Item => "Done initilizing variables");
-   Ada.Text_IO.Open (File => In_File, Mode => Ada.Text_IO.In_File, Name => "/home/moshell_jw/alice.txt");
+	
+	--filename := ada.command_line.Argument(1);
+	
+	--tokenizer begin
+   --Ada.Text_IO.Put (Item => "Done initilizing variables");
+   Ada.Text_IO.Open (File => In_File, Mode => Ada.Text_IO.In_File, Name => "/pub/pounds/CSC330/translations/KJV.txt");
 
 	--for each
    pos := 0;
@@ -49,10 +65,91 @@ begin
    end loop;
 
   for s of v loop
-   put(s);
-   new_line;
-  end loop;
-
-   Ada.Text_IO.New_Line;
-
+	validword := False;
+	--put("."); put(s); put_line(".");
+	--tokenizer end
+		if s'length = 0 then
+			goto tokEnd;
+		end if;
+		--punctuation
+		for j in s'range loop
+			if ASM.is_in(s(j), ASM.to_set(".;:?!")) then
+				if(validsent) then
+					sentcount := sentcount + 1;
+				end if;
+				validsent := false;
+			end if;
+		end loop;
+		--fix token
+		--lowercasize
+		s := ACH.to_lower(s);
+		--put(s); put(" / ");
+		--strip
+		writeh := 1;
+		for k in s'range loop
+			--put(k);
+			if not ACH.is_letter(s(k)) then
+				ASF.delete(s, k, k);
+			else
+				validword := true;
+			end if;
+		end loop;
+		--put(s);
+		--new_line;
+		
+		--analyze
+		if validword then
+			wordcount := wordcount + 1;
+			validsent := true;
+		else
+			goto tokEnd;
+		end if;
+		
+		--vowel
+		syllinword := 0;
+		lchvow := false;
+		for l in s'range loop
+			if ASM.is_in(s(l), ASM.to_set("aeiouy")) then
+				if not lchvow then
+					syllinword := syllinword + 1;
+				end if;
+				lchvow := true;
+			else
+				lchvow := false;
+			end if;
+		end loop;
+		if s(s'last) = 'e' then
+			syllinword := syllinword - 1;
+		end if;
+		if syllinword < 1 then
+			syllinword := 1;
+		end if;
+		syllcount := syllcount + syllinword;
+		
+		<<tokEnd>>
+	end loop;
+	
+	--calculate scores
+	alpha := float(syllcount) / float(wordcount);
+	beta := float(wordcount) / float(sentcount);
+	
+	result := float'rounding(206.835 - alpha*84.6 - beta*1.015);
+	--put(syllcount); new_line; put(wordcount); new_line; put(sentcount); new_line;
+	put("Flesch Index: "); put(integer(result)'image); new_line;
+	
+	result := (alpha*11.8+beta*0.39-15.59);
+	put("Flesch-Kincaid Index: "); ada.float_text_io.put(result, 2, 1, 0); new_line;
+	
 end flesch;
+
+
+
+
+
+
+
+
+
+
+
+
